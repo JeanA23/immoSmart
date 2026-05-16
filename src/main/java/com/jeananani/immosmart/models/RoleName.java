4760549f72c5bbb8554829a0f055eaabb5e90cc1
Jean Anani <ananijean23@gmail.com>
2026-05-16 18:37:47 +0200

    feat(entities): add user and role entities and rolename enum

---
+++ src/main/java/com/jeananani/immosmart/models/RoleName.java
@@ -0,0 +1,8 @@
+package com.jeananani.immosmart.models;
+
+public enum RoleName {
+
+    ADMIN,
+    SELLER,
+    USER
+}
---
+++ src/main/java/com/jeananani/immosmart/models/User.java
@@ -0,0 +1,73 @@
+package com.jeananani.immosmart.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String firstName;
+
+    @Column(nullable = false, length = 100)
+    private String lastName;
+
+    @Column(name = "username", nullable = false, length = 100)
+    private String username;
+
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    @Column(unique = true, nullable = false, length = 150)
+    private String email;
+
+    @Column(nullable = false, length = 10)
+    private String phone;
+
+    @Column(nullable = false)
+    @ToString.Exclude
+    private String password;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Boolean active = true;
+
+    @Enumerated(EnumType.STRING)
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    Set<Role> roles = new HashSet<>();
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
