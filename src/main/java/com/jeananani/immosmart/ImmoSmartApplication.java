2fcb4b6aa10e5fae3679c36e44d7f26e2ab2ab23
Jean Anani <ananijean23@gmail.com>
2026-05-16 17:17:38 +0200

    chore: initial project setup

---
+++ src/main/java/com/jeananani/immosmart/ImmoSmartApplication.java
@@ -0,0 +1,13 @@
+package com.jeananani.immosmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmoSmartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImmoSmartApplication.class, args);
+    }
+
+}
